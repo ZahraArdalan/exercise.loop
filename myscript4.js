0811c5d1549568970cdaff96268339fb887e6389
zahra <z.ardalan78@gmail.com>
2024-09-24 19:56:39 +0330

    js4

---
+++ myscript4.js
@@ -0,0 +1,10 @@
+for (let exercise = 1; exercise < 4;  exercise++){
+   console.log(`-------starting exercise ${exercise}`);
+
+
+   for (let rep = 1; rep < 6; rep++) {
+    console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+    
+   }
+  
+}
